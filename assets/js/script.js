f2384ecb8b5bc95a46cc9f2ab45bf9461b794279
hmanjun <harshith.hm03@gmail.com>
2022-07-04 10:15:07 -0700

    Initial dynamic-pantry branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,12 @@ $(document).on("click", ".close", function () {
     pantryModal.css("display", "none")
 });
 
+/*Get List of ingredients*/
+
+
+
+/* API ingredient check
+
+https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=597241d5914540eb9a064d99f044c672&query=god&number=5
+
+*/
