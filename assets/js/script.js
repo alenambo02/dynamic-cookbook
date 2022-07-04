35dc5b03ee8d47ed5a8ae6dbfa206e25159d0c38
hmanjun <harshith.hm03@gmail.com>
2022-07-04 10:25:39 -0700

    Add panty input validation via api call to check if real ingredient

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,18 @@ $(document).on("click", ".close", function () {
 
 /*Get List of ingredients*/
 
-$(document).on("click", "#addItemBtn", function(event){
-    
+$(document).on("click", "#addItemBtn", function(event){ //Add ingredient listener
+    var itemVal = $(this).siblings("input").val()
+    if(itemVal != ""){ //Simple validation for if any input at all
+        var itemCheckUrl = "https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=597241d5914540eb9a064d99f044c672&query=" + itemVal
+        $.ajax({ //Fetch validation. Call api to see if an ingredient exsists with the name of the input
+            url: itemCheckUrl,
+            method: "GET"
+        })
+            .then(function(response){
+                console.log(response)
+            })
+    }
 })
 
 /* API ingredient check
