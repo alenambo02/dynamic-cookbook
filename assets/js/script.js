7de4829e98b7978d4391383c913ca3f981846415
priyadarshini <priya.rizal2@gmail.com>
2022-07-09 12:57:53 -0400

    accidentally was working on main branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,33 +116,4 @@ function itemNotAValidInput() {
     }, 1000)
 }
 
-//Priya's JS 
 
-var seconPage = document.querySelector('#secondPage')
-
-var granimInstance = new Granim({
-    element: '#canvas-image-blending',
-    direction: 'top-bottom',
-    isPausedWhenNotInView: true,
-    image: {
-        // source: '../assets/img/bg-forest.jpg',
-        blendingMode: 'multiply',
-    },
-    states: {
-        "default-state": {
-            gradients: [
-                ['#29323c', '#485563'],
-                ['#FF6B6B', '#556270'],
-                ['#80d3fe', '#7ea0c4'],
-                ['#f0ab51', '#eceba3']
-            ],
-            transitionSpeed: 7000
-        }
-    }
-});
-
-function loadPage (){
-
-    landingPage = document.querySelector('#landingpage')
-    seconPage.style.display = 'none'
-}
