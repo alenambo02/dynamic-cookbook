c5521a91a40ec462540c8ecfde7c68482cce1e3f
priyadarshini <priya.rizal2@gmail.com>
2022-07-09 13:49:46 -0400

    granim wasn't working/ trying again

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,15 +1,43 @@
 
+// var granimInstance = new Granim({
+//     element: '#granim',
+//     name: 'granimFirst',
+//     elToSetClassOn: ".wrapper",
+//     direction: 'top-bottom',
+//     isPausedWhenNotInView: true,
+//     image: {
+//         source: './images/3.png',
+//         stretchMode: ["stretch-if-smaller", "stretch-if-smaller"],
+
+//         blendingMode: 'multiply',
+//     },
+//     states: {
+//         "default-state": {
+//             gradients: [
+//                 ['#29323c', '#485563'],
+//                 ['#FF6B6B', '#556270'],
+//                 ['#80d3fe', '#7ea0c4'],
+//                 ['#f0ab51', '#eceba3']
+//             ],
+//             transitionSpeed: 7000
+//         }
+//     }
+// });
+
+
+$(".clickMe").on("click", function () {
+    window.location = "./index.html"
+});
+
 var granimInstance = new Granim({
     element: '#granim',
-    name: 'granimFirst',
-    elToSetClassOn: ".wrapper",
     direction: 'top-bottom',
+  
     isPausedWhenNotInView: true,
     image: {
-        source: './images/3.png',
+        source: './images/5.png',
         stretchMode: ["stretch-if-smaller", "stretch-if-smaller"],
-
-        blendingMode: 'multiply',
+        blendingMode: 'multiply'
     },
     states: {
         "default-state": {
@@ -22,10 +50,5 @@ var granimInstance = new Granim({
             transitionSpeed: 7000
         }
     }
-});
-
-
-$(".clickMe").on("click", function () {
-    window.location = "./index.html"
-});
-
+ });
+ 
