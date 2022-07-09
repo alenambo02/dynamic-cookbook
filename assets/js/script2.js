2f16fde4f290e4a1006c27bffed7db77008eeceb
priyadarshini <priya.rizal2@gmail.com>
2022-07-09 13:29:44 -0400

    granim css/JS work in progress

---
+++ assets/js/script2.js
@@ -0,0 +1,9 @@
+
+var granimInstance = new Granim({
+    element: '#granim',
+    name: 'granimFirst',
+    elToSetClassOn: ".wrapper",
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+
+});
